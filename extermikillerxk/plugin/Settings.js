19d510f79b8b5a32746178399a9ca4cc477fb508
ExtermikillerXK <78577700+ExtermikillerXK@users.noreply.github.com>
2024-07-19 20:55:29 -0400

    Create Settings.js

---
+++ extermikillerxk/plugin/Settings.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const Settings = ({ updateSettings, settings }) => {
+    const [commandPrefix, setCommandPrefix] = useState(settings.commandPrefix || "!");
+
+    const handlePrefixChange = (event) => {
+        const newPrefix = event.target.value;
+        setCommandPrefix(newPrefix);
+        updateSettings({ commandPrefix: newPrefix });
+    };
+
+    return (
+        <div>
+            <label>
+                Command Prefix:
+                <input type="text" value={commandPrefix} onChange={handlePrefixChange} />
+            </label>
+        </div>
+    );
+};
+
+export default Settings;
